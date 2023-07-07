c642e6322b92f299750068197c215d1065b3622f
hafidz.izzudin <hafidz.izzudin49@gmail.com>
2023-07-07 14:50:32 +0700

    feat: integrate with localhost get color

--- src/app/color.service.ts
+++ src/app/color.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 
@@ -8,20 +9,37 @@ export class ColorStateService {
     private _color = new BehaviorSubject<string>("#FFFFFF")
     private _colorList = ['orangered', 'red', 'green', 'blue', '#452']
 
+    constructor(private httpClient: HttpClient) { }
+
     get color$() {
         return this._color.asObservable();
     }
 
     updateColor() {
-        let index = this.getRandom(this._colorList.length)
-        while (this._colorList[index] === this._color.getValue()) {
-            index = this.getRandom(this._colorList.length)
-        }
-        console.log(index, this._colorList[index]);
-        this._color.next(this._colorList[index]);
+        // this.updateColorTest();
+        this.updateColorFromRest();
     }
 
-    private getRandom(val: number): number {
-        return Math.floor(Math.random() * val)
+    private updateColorTest() {
+        let val = this._colorList.length;
+        let index = Math.floor(Math.random() * val)
+
+        while (this._colorList[index] === this._color.getValue()) {
+            index = Math.floor(Math.random() * val)
+        }
+        console.log(index, this._colorList[index]);
+
+        this._color.next(this._colorList[index])
+    }
+
+    private updateColorFromRest() {
+        this.httpClient.get('http://localhost:3000/color')
+            .subscribe({
+                next: (data: any) => {
+                    console.log(data);
+                    this._color.next(data.color);
+                },
+                error: (error) => console.error(error)
+            });
     }
 }
